3c29a8965537424887d480afc70874d47e5ffd3e
mdarcy <mdarcy@22429d68-4e6d-4b4f-8b83-96928a890c38>
2013-03-26 00:15:09 +0000

    added an additional GLORE test using the MTM simulation data that can be used to recreate the condition where the analysis is running more than 15K iterations before running out of memory

---
+++ trunk/network/modules/glore/test/src/test/java/edu/isi/misd/scanner/network/modules/test/glore/GloreVariableTest.java
@@ -0,0 +1,63 @@
+package edu.isi.misd.scanner.network.modules.test.glore;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class GloreVariableTest extends GloreIntegrationTest 
+{       
+    @Override
+    protected String getWorkerUrls() throws Exception
+    {
+        String targets =              
+            context.resolvePropertyPlaceholders(
+            "http4://{{worker.address}}:{{worker.port}}/{{worker.appDomain}}/{{worker.appContext}}/glore/lr?dataSource=MTM_SIMULATED_1.csv,"+
+            "http4://{{worker.address}}:{{worker.port2}}/{{worker.appDomain}}/{{worker.appContext}}/glore/lr?dataSource=MTM_SIMULATED_2.csv,"+
+            "http4://{{worker.address}}:{{worker.port3}}/{{worker.appDomain}}/{{worker.appContext}}/glore/lr?dataSource=MTM_SIMULATED_3.csv");
+        return targets;
+    }  
+    
+    @Test
+    @Override
+    public void testGloreLogisticRegressionXML() throws Exception 
+    {
+        doPost("application/xml",
+               "GloreVariableTestInput1.xml",
+               "GloreVariableTestOutput1.xml");
+        assertMockEndpointsSatisfied(120, TimeUnit.SECONDS);         
+    }
+    
+    @Test
+    @Override
+    public void testGloreLogisticRegressionJSON() throws Exception 
+    {
+        doPost("application/json",
+               "GloreVariableTestInput1.json",
+               "GloreVariableTestOutput1.json");
+        assertMockEndpointsSatisfied(120, TimeUnit.SECONDS);   
+    }     
+    
+    @Test
+    public void testGloreVariablesXML() throws Exception 
+    {
+//        doPost("application/xml",
+//               "GloreVariableTestInput1.xml",
+//               "GloreVariableTestOutput1.xml");
+        doPost("application/xml",
+               "GloreVariableTestInput2.xml");        
+        assertMockEndpointsSatisfied(60, TimeUnit.MINUTES);         
+    }
+    
+//    @Test
+//    public void testGloreVariablesJSON() throws Exception 
+//    {
+////        doPost("application/json",
+////               "GloreVariableTestInput1.json",
+////               "GloreVariableTestOutput1.json");
+//        doPost("application/json",
+//               "GloreVariableTestInput2.json");        
+//        assertMockEndpointsSatisfied(60, TimeUnit.MINUTES);   
+//    }         
+}
