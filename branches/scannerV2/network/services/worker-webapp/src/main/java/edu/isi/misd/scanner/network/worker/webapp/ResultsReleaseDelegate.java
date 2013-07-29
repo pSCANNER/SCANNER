59f6696b04a1c03ad0e4dc201ab35985b60b9eaa
mdarcy <mdarcy@22429d68-4e6d-4b4f-8b83-96928a890c38>
2013-07-29 19:51:47 +0000

    Fix Activiti delegate code so that the file is deleted from the holding area in case of a rejection. Log the successful events of moving or deleting the file in addition to the failures of those events.

--- branches/scannerV2/network/services/worker-webapp/src/main/java/edu/isi/misd/scanner/network/worker/webapp/ResultsReleaseDelegate.java
+++ branches/scannerV2/network/services/worker-webapp/src/main/java/edu/isi/misd/scanner/network/worker/webapp/ResultsReleaseDelegate.java
@@ -14,6 +14,8 @@ import org.slf4j.LoggerFactory;
  */
 public class ResultsReleaseDelegate implements JavaDelegate 
 {  
+    public static final String ID = "id";    
+    public static final String APPROVED = "approved";
     public static final String FILE_PATH = "filePath";
     public static final String HOLDING_PATH = "holdingPath";
 
@@ -23,6 +25,16 @@ public class ResultsReleaseDelegate implements JavaDelegate
     @Override
     public void execute(DelegateExecution execution) throws Exception 
     {
+        if (!execution.hasVariable(ID)) {
+            throw new Exception(
+                "Variable not present: " + ID);            
+        }
+        
+        if (!execution.hasVariable(APPROVED)) {
+            throw new Exception(
+                "Variable not present: " + APPROVED);            
+        }
+        
         if (!execution.hasVariable(FILE_PATH)) {
             throw new Exception(
                 "Variable not present: " + FILE_PATH);            
@@ -37,6 +49,22 @@ public class ResultsReleaseDelegate implements JavaDelegate
             new File((String)execution.getVariable(FILE_PATH));       
         File holdingFile = 
             new File((String) execution.getVariable(HOLDING_PATH));
+        
+        String id = (String)execution.getVariable(ID);
+        String approved = (String)execution.getVariable(APPROVED);
+        if (!Boolean.parseBoolean(approved)) {
+            try {
+                FileUtils.forceDelete(holdingFile);
+                log.info("Release approval rejected for request [" + id + "]." + 
+                         "  The following file will be deleted: " +
+                         holdingFile.getCanonicalPath());
+            } catch (Exception e) {
+                log.warn("Could not delete output file from holding directory: " + 
+                          holdingFile.getCanonicalPath());  
+                throw e;
+            }
+            return;
+        }
         
         if (outputFile.exists()) {
             log.warn(
@@ -53,6 +81,10 @@ public class ResultsReleaseDelegate implements JavaDelegate
         
         try {
             FileUtils.moveFile(holdingFile,outputFile);
+            log.info("Release approval accepted for request [" + id + "]." + 
+                     "  The results file was moved from " +
+                     holdingFile.getCanonicalPath() + " to " +
+                     outputFile.getCanonicalPath());            
         } catch (Exception e) {
             log.warn(
                 "Unable to move file " + holdingFile.getCanonicalPath() + 
