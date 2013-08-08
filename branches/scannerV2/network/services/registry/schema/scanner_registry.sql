b9a54400546cfa2860fcaf918073e96adf58840e
laura <laura@22429d68-4e6d-4b4f-8b83-96928a890c38>
2013-08-08 23:46:11 +0000

    Some renames and fixing a unique index

--- branches/scannerV2/network/services/registry/schema/scanner_registry.sql
+++ branches/scannerV2/network/services/registry/schema/scanner_registry.sql
@@ -3,12 +3,13 @@ set search_path = scanner_registry;
 
 create table if not exists tool_library (
   library_id serial not null primary key,
-  libary_name text not null unique,
+  library_name text not null unique,
   library_version text not null,
-  description text
+  description text,
+  unique(library_name, library_version)
 );
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS scanner_user (
   user_id serial NOT NULL primary key,
   username text not null unique,
   email text NOT NULL,
@@ -16,7 +17,7 @@ CREATE TABLE IF NOT EXISTS users (
   Primary_Affliation integer NOT NULL,
   Secondary_Affliliation integer NOT NULL,
   Phone text NOT NULL,
-  Reports_To integer NOT NULL references users(user_id),
+  Reports_To integer NOT NULL references scanner_user(user_id),
   Active boolean NOT NULL,
   First_Name text NOT NULL,
   Middle_Initial text,
@@ -24,11 +25,11 @@ CREATE TABLE IF NOT EXISTS users (
   PubMed_Author_ID text
 );
 
-CREATE TABLE IF NOT EXISTS study(
+CREATE TABLE IF NOT EXISTS study (
   Study_ID serial NOT NULL primary key,
   IRB_ID integer NOT NULL,
   Protocol text NOT NULL,
-  Principal_Investigator_UID integer NOT NULL references users(user_id),
+  Principal_Investigator_UID integer NOT NULL references scanner_user(user_id),
   Start_Date date NOT NULL,
   End_Date date NOT NULL,
   Clinical_Trials_ID integer NOT NULL,
@@ -42,23 +43,23 @@ comment on column study.grant_ids is 'Daniella: actually need a table Grant ID-S
 comment on column study.data_set_ids is 'Daniella: Need a table Data Set ID-Study ID';
 comment on column study.dua_ids is 'Daniella: Need a table DUA-study';
 
-create table if not exists roles (
+create table if not exists scanner_role (
   role_id serial not null primary key,
   study_id integer not null references study(study_id),
   role_within_study text not null,
   unique(study_id, role_within_study)
 );
 
-COMMENT on column roles.role_within_study is 'PI, CO-I, PM, etc.';
+COMMENT on column scanner_role.role_within_study is 'PI, CO-I, PM, etc.';
 
-CREATE TABLE IF NOT EXISTS investigator_roles (
-  Investigator_ID integer NOT NULL references users(user_id),
-  role_id integer NOT NULL references roles(role_id),
+CREATE TABLE IF NOT EXISTS investigator_role (
+  Investigator_ID integer NOT NULL references scanner_user(user_id),
+  role_id integer NOT NULL references scanner_role(role_id),
   primary key (Investigator_ID, role_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS analysis_tools (
+CREATE TABLE IF NOT EXISTS analysis_tool (
   Tool_ID serial NOT NULL primary key,
   tool_name text not null,
   Tool_Parent_Library_ID integer NOT NULL references tool_library(library_id),
@@ -70,14 +71,14 @@ CREATE TABLE IF NOT EXISTS analysis_tools (
   unique (tool_name, tool_parent_library_id)
 );
 
-create table confidentiality_levels (
+create table confidentiality_level (
   level_id serial not null primary key,
   level_name text not null unique
 );
 
-insert into confidentiality_levels(level_name) values ('SAFE HARBOR'), ('DUA-COVERED LIMITED DATA SET'), ('IDENTIFIED DATA');
+insert into confidentiality_level(level_name) values ('SAFE HARBOR'), ('DUA-COVERED LIMITED DATA SET'), ('IDENTIFIED DATA');
 
-create table data_set_policy_authorities (
+create table data_set_policy_authority (
   authority_id serial not null primary key,
   authority_name text not null unique
 );
@@ -87,9 +88,9 @@ CREATE TABLE IF NOT EXISTS data_set_definition (
   Data_Description_XML text NOT NULL,
   Data_Processing_XML text NOT NULL,
   Data_Processing_Program text NOT NULL,
-  Author_UID integer NOT NULL references users(user_id),
+  Author_UID integer NOT NULL references scanner_user(user_id),
   Originating_Study_ID integer NOT NULL references study(study_id),
-  Data_Set_Confidentiality_Level integer NOT NULL references confidentiality_levels(level_id)
+  Data_Set_Confidentiality_Level integer NOT NULL references confidentiality_level(level_id)
 );
 
 COMMENT on column data_set_definition.data_description_xml is 'Daniella: Path to XML describing data';
@@ -101,9 +102,9 @@ COMMENT on column data_set_definition.data_set_confidentiality_level is 'Daniell
 
 CREATE TABLE IF NOT EXISTS source_data_warehouse (
   source_data_warehouse_id serial not null primary key,
-  Connectivity_Manager_ID integer NOT NULL references users(user_id),
-  Data_Manager_ID integer NOT NULL references users(user_id),
-  data_warehouse_confidentiality_level integer not null references confidentiality_levels(level_id),
+  Connectivity_Manager_ID integer NOT NULL references scanner_user(user_id),
+  Data_Manager_ID integer NOT NULL references scanner_user(user_id),
+  data_warehouse_confidentiality_level integer not null references confidentiality_level(level_id),
   Schema_Documentation text NOT NULL,
   ETL_Documentation text NOT NULL,
   ETL_Programs text NOT NULL
@@ -116,25 +117,25 @@ CREATE TABLE IF NOT EXISTS data_set_instance (
   Data_Set_Instance_ID serial NOT NULL primary key,
   Data_Set_Definition_ID integer NOT NULL references data_set_definition(data_set_definition_id),
   Data_Set_Instance_Location text NOT NULL,
-  Curator_UID integer NOT NULL references users(user_id),
+  Curator_UID integer NOT NULL references scanner_user(user_id),
   Study_ID integer NOT NULL references study(study_id),
   Source_Data_Warehouse_ID integer NOT NULL references source_data_warehouse (source_data_warehouse_id),
   Data_Slice_ID integer DEFAULT NULL
 );
 
-create table if not exists access_modes (
+create table if not exists access_mode (
   access_mode_id serial not null primary key,
   access_mode_name text not null unique
 );
 
-insert into access_modes(access_mode_name) values ('synchronous'), ('asynchronous');
+insert into access_mode(access_mode_name) values ('synchronous'), ('asynchronous');
 
 create table if not exists policy_statement (
   policy_statement_id serial not null primary key,
   data_set_instance_id integer not null references data_set_instance(data_set_instance_id),
-  role_id integer not null references roles(role_id),
-  analysis_tool_id integer not null references analysis_tools(tool_id),
-  access_mode integer not null references access_modes(access_mode_id)
+  role_id integer not null references scanner_role(role_id),
+  analysis_tool_id integer not null references analysis_tool(tool_id),
+  access_mode integer not null references access_mode(access_mode_id)
 );
 
 COMMENT on table policy_statement is 'Users in role <role_id> may run toll <tool_id> on data set instance <data_set_instance_id> in mode <access_mode>';
@@ -145,18 +146,3 @@ COMMENT on table policy_statement is 'Users in role <role_id> may run toll <tool
 -- it's not clear how those map to individual policy statements, or how the assertion would be tied to the person making the assertion.
 -- Some other columns now available as joins to other tables: data resource definition id, data resource confidentiality level, and
 -- data source (which I assume is the source data warehouse) are in the data set instance table; study id is in the roles table).
-
-
-CREATE TABLE IF NOT EXISTS dua_study(
-  DUA_ID integer NOT NULL,
-  Study_ID integer NOT NULL,
-  primary key (DUA_ID,Study_ID)
-);
-
-COMMENT on TABLE dua_study is 'Daniella: Table linking the DUA to the study';
-
-
-
-
-
-
