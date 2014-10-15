30b596cd268fe6bcba2ed5c755e0435430f89236
png@rand.org <png@rand.org>
2014-10-15 18:01:50 +0000

    Added formatting of date time.

--- network/services/portal/src/main/webapp/dataProcessor/scripts/hqmf/measurePeriodBuilder.js
+++ network/services/portal/src/main/webapp/dataProcessor/scripts/hqmf/measurePeriodBuilder.js
@@ -20,12 +20,33 @@ define(['hqmf/rangeConstraint'], function (RangeConstraint)
                     else if ( app.initialPopulation.models[k].attributes.type === 'endDate' )
                         j = k;
                 }
-                var lowValue = {type:'TS', value: app.initialPopulation.models[i].attributes.name, 'inclusive?':true, 'derived?':false};
-                var highValue = {type:'TS', value: app.initialPopulation.models[j].attributes.name, 'inclusive?':true, 'derived?':false};
+                var lowValue = {type:'TS', value: this.formatDateTime(app.initialPopulation.models[i].attributes.name), 'inclusive?':true, 'derived?':false};
+                var highValue = {type:'TS', value: this.formatDateTime(app.initialPopulation.models[j].attributes.name), 'inclusive?':true, 'derived?':false};
                 period = RangeConstraint.getInstance();
                 period.initialize('IVL_TS', highValue, lowValue);
             }
             return period;
+        },
+        // Converts dateTimeString to HQMF format (YYYYMMDDhhmm).
+        formatDateTime: function(dateTimeString) 
+        {
+                var d = new Date(dateTimeString);
+                var year = d.getFullYear().toString();
+                var month = (d.getMonth()+1).toString();
+                // Pad with leading zero if less than 2 digits.
+                var day = d.getDate().toString();
+                if ( day.length === 1 )
+                    day = '0'+day;
+                // Pad with leading zero if less than 2 digits.
+                var hours = d.getHours().toString();
+                if ( hours.length === 1 )
+                    hours = '0'+hours;
+                // Pad with leading zero if less than 2 digits.
+                var minutes = d.getMinutes().toString();
+                if ( minutes.length === 1 )
+                    minutes = '0'+minutes;
+                
+            return year+month+day+hours+minutes;
         }
     };
     return result;
